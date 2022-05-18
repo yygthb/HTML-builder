b39cf70f72f885b4b61eba45ee9e186d8b1f29ca
Yura Ya <yy.ainu.dev@gmail.com>
2022-05-18 13:54:33 +0300

    feat: 01 read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+
+const FILE_NAME = 'text.txt';
+const FILE_PATH = path.resolve(__dirname, FILE_NAME);
+
+const stream = fs.createReadStream(FILE_PATH, { encoding: 'utf-8' });
+
+// stream.on('data', (data) => console.log(data));
+stream.on('data', (data) => process.stdout.write(data));
+stream.on('error', (err) => console.log(err));
