ec8f6623e6bf6a77ff077f358dde119c200698d6
Yura Ya <yy.ainu.dev@gmail.com>
2022-05-19 14:59:22 +0300

    feat: 03 files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs = require('fs');
+
+const DIR_NAME = 'secret-folder';
+const DIR_PATH = path.resolve(__dirname, DIR_NAME);
+
+const READDIR_CONFIG = {
+  encoding: 'utf-8',
+};
+
+fs.readdir(DIR_PATH, READDIR_CONFIG, (err, files) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  for (let file of files) {
+    getFileInfo(file);
+  }
+});
+
+async function getFileInfo(file) {
+  const [success, fileSize] = await isFile(file);
+
+  if (success) {
+    const fileName = file.split('.')[0];
+    const fileExt = getFileExt(file);
+
+    console.log(`${fileName} - ${fileExt} - ${fileSize}b`);
+  }
+}
+
+function isFile(fileName) {
+  const filePath = path.resolve(DIR_PATH, fileName);
+  return new Promise((res, rej) => {
+    fs.lstat(filePath, (err, stats) => {
+      if (err) rej(err);
+
+      res([stats.isFile(), stats.size]);
+    });
+  });
+}
+
+function getFileExt(fileName) {
+  const fileExt = path.parse(path.resolve(DIR_PATH, fileName)).ext;
+  return fileExt.slice(1);
+}
